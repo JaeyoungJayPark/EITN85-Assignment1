e0548885ea3d939c3ec09788cd6a3354d9b1ae32
Jaeyoung Park <59507131+JaeyoungJayPark@users.noreply.github.com>
2022-02-22 19:35:56 +0100

    Update Assignment1_2022.m

--- Assignment1_2022.m
+++ Assignment1_2022.m
@@ -21,7 +21,6 @@ for i=6:5:length(ComplexAmplitude)-5
      ssfamplitude=[ssfamplitude ratio]; %Small-scale fading amplitude based on the small-scale averaged power. 
 end
 
-
 figure(1) %new figure window
 semilogx((TxRxDist),10*log10(RxPow),'k.') %plot distance against received power, with logarithmic x-axis.
 xlabel('Tx-Rx separation distance [m]', 'FontSize', 13, 'Interpreter', 'latex') %label for the x-axis
@@ -34,6 +33,7 @@ semilogx((D_win),10*log10(P_SSA),'bo') %Plot small-scale averaged channel gain
 semilogx(([10 200]),-20*log10(4*pi*[10 200]/(3e8/2.6e9)),'r-')
 %A legend of for window 1:
 legend('Channel gain','Small-scale averaged channel gain','Free space channel gain', 'Location', 'best', 'Location', 'best', 'Interpreter','latex', 'Fontsize', 11)
+
 %--------------------------------------------------
 
 %A2: Find the ordinary least-squares estimate of PL(d0)
@@ -42,9 +42,12 @@ d_0 = 1;
 fun = @(x,D_win)x(1)-10*x(2)*log10(D_win/d_0); %P(d) = P(d_0) - 10*n*log(10/d_0)
 x = lsqcurvefit(fun,[0,0],D_win,10*log10(P_SSA));
 
-PL_d = x(1) - 10*x(2)*log10(D_win/d_0);
+Pl_d0 = x(1); 
+n = x(2);
+PL_d = Pl_d0 - 10*n*log10(D_win/d_0);
 
 %Plot the average received power using the pathloss estimates found above: 
+
 figure(2)
 xlabel('Tx-Rx separation distance [m]', 'FontSize', 13, 'Interpreter', 'latex') %label for the x-axis
 ylabel('Channel gain [dB]', 'FontSize', 13, 'Interpreter', 'latex') %label for the y-axis.
@@ -52,13 +55,14 @@ grid on %add grid
 hold on %hold on, to be able to plot another figure, while keeping the old plot.
 plot((D_win),10*log10(P_SSA),'bo',D_win,fun(x,D_win),'g-', 'LineWidth', 1.2)
 legend('Small-scale averaged channel gain','OLS fitted curve', 'Location', 'best', 'Location', 'best', 'Interpreter','latex', 'Fontsize', 11)
+
 %--------------------------------------------------
 
 %A3: In a new window, plot the empirical cdf of the large-scale fading from
 %the measurement:
 figure(3)
 
-LSF = P_SSA - PL_d;
+LSF = 10*log10(P_SSA) - PL_d;
 
 %Plot the empirical cumulative distribution funcfion
 e_LSF = cdfplot(LSF); e_LSF.LineStyle = 'none'; e_LSF.Marker = '.';
@@ -66,12 +70,16 @@ hold on; grid on;
 
 %Plot the CDF for the normal distribution based on the maximum-likelihood estimate
 %for a normal distribution: 
+
+
 N_LSF = length(LSF);
 
 mean_LSF = 1/N_LSF * sum(LSF);
-var_LSF = 1/N_LSF * sum((LSF - mean_LSF).^2);
+var_LSF2 = 1/N_LSF * sum((LSF - mean_LSF).^2);
+var_LSF = sqrt(var_LSF2);
 
-CDF_Normal_LSF = (1/2) * (1+erf((LSF-mean_LSF)/(var_LSF*2^(-2))));
+CDF_Normal_LSF = (1/2) * (1+erf((LSF-mean_LSF)/(var_LSF*sqrt(2))));
+%cdf_nd = 0.5*(1+erf((LSF-mean_LSF)/(variance_LSF*sqrt(2)) ));
 plot(LSF, CDF_Normal_LSF, '.')
 legend('Emprical', 'Gaussian Modelled', 'Location', 'best', 'Interpreter','latex', 'Fontsize', 11)
 xlabel('Large Scale Fading [dB]', 'FontSize', 13, 'Interpreter', 'latex') %label for the x-axis
@@ -101,8 +109,8 @@ ylabel('cumulative distribution function of $SSF_{amp}$', 'FontSize', 13, 'Inter
 title('\textbf{CDF of Small Scale Fading Amplitude}', 'Interpreter', 'latex', 'FontSize', 14)
 %-------------------------------------------------- 
 
-%--------------------------------------------------
 
+%--------------------------------------------------
 %A5: Plot the cdf for the Rice distribution with the
 %parameter estimates given in the assignment. Hint:
 %Type "help marcumq".
